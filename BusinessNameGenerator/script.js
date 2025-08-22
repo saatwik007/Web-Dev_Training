1e7e6c8a68878a7c9654fcac20510deb12d2968e
saatwik wahi <saawahi@deloitte.com>
2025-08-22 23:32:15 +0530

    Day - 6  22/8/25

---
+++ BusinessNameGenerator/script.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+let a = "crazy";
+b = "Amazing";
+c= "Fire";
+d='Engine';
+e='foods';
+f='garments';
+g='bros'
+h='limited'
+i='hub'
+
+let Random1=[Math.floor(Math.random()*3)+1];
+let Random2=[Math.floor(Math.random()*3)+4];
+let Random3=[Math.floor(Math.random()*3)+7];
+console.log(Random1, Random2, Random3);
