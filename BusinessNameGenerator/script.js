48bcd3a062909519e83fbc94baa42a229d02306e
saatwik007 <saatwikwahi100@gmail.com>
2025-08-22 23:36:08 +0530

    Day - 7  22/8/25

--- BusinessNameGenerator/script.js
+++ BusinessNameGenerator/script.js
@@ -1,16 +1,20 @@
 console.log("Hello World");
 
 let a = "crazy";
-b = "Amazing";
-c= "Fire";
-d='Engine';
-e='foods';
-f='garments';
-g='bros'
-h='limited'
-i='hub'
+let b = "Amazing";
+let c= "Fire";
+let d='Engine';
+let e='foods';
+let f='garments';
+let g='bros'
+let h='limited'
+let i='hub'
 
 let Random1=[Math.floor(Math.random()*3)+1];
 let Random2=[Math.floor(Math.random()*3)+4];
 let Random3=[Math.floor(Math.random()*3)+7];
 console.log(Random1, Random2, Random3);
+
+if (Random1==1){
+    console.log(a);
+}
