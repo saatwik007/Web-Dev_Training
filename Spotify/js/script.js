75e19c7b13d8c185a8704d0e6f69988f92dbbe08
saatwik007 <saatwikwahi100@gmail.com>
2025-09-03 22:53:04 +0530

    Day - 17  3/9/25 - Spotify Start day - 2

---
+++ Spotify/js/script.js
@@ -0,0 +1,4 @@
+console.log("Hello, Spotify!");
+// const audioPlayer = document.getElementById('audioPlayer');
+// audioPlayer.src = 'songs/song1.mp3'; // Change to your song path
+// audioPlayer.play();
