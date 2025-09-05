0f9d2326ba829abbeaff03b49a54fa60ed0342c8
saatwik007 <saatwikwahi100@gmail.com>
2025-09-05 23:00:23 +0530

    Day - 19  5/9/25 - Spotify Start day - 4

--- Spotify/js/script.js
+++ Spotify/js/script.js
@@ -2,3 +2,42 @@ console.log("Hello, Spotify!");
 // const audioPlayer = document.getElementById('audioPlayer');
 // audioPlayer.src = 'songs/song1.mp3'; // Change to your song path
 // audioPlayer.play();
+
+async function getSongs(){
+    let a = await fetch('http://127.0.0.1:5500/Spotify/songs/');
+    let response = await a.text();
+    let div = document.createElement('div');
+    div.innerHTML = response;
+    let as = div.getElementsByTagName('a');
+    let song=[]
+    for(let i=0;i<as.length;i++){
+        const element = as[i];
+        if(element.href.endsWith(".mp3")){
+            song.push(element.href);
+        }
+    }
+    console.log(song);
+    return song;
+}
+
+
+async function main(){
+    let songs = await getSongs();
+    console.log(songs);
+    
+}
+main();
+// let addSong = document.getElementById("songs");
+// addSong.innerHTML = `
+//     <li><p>First Song</p></li>
+//     <li><p>Second Song</p></li>
+//     <li><p>Third Song</p></li>
+// `;
+// songs.forEach(song => {
+//   const songItem = document.getElementById('songs');
+//   songItem.innerHTML += `
+//     <li>
+//       <p>${song}</p>
+//     </li>
+//   `;
+// });
