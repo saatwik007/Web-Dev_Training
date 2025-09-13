d3519784adbc3e2c88acff5b56789edb419d2c60
saatwik007 <saatwikwahi100@gmail.com>
2025-09-14 04:17:36 +0530

    Day - 27  14/9/25 - Node

--- TempNode/server.js
+++ TempNode/server.js
@@ -1,12 +1,12 @@
-// const { createServer } = require('node:http');
-import http from 'http';
-// const hostname = '127.0.0.1';
-// const port = 3000;
-// const server = createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/html');
-//   res.end('<button>Click Me</button>');
-// });
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
+const { createServer } = require('node:http');
+// import http from 'http';
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<button>Click Me</button>');
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
