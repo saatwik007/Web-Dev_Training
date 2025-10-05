9bdba3ac39fa25622b86b4f8d5a84a0699e4b674
saatwik007 <saatwikwahi100@gmail.com>
2025-10-05 22:26:51 +0530

    Day - 47  5/10/25 - REACT

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  
+  const [a, changeState] = useState(0); 
+  
+  useEffect(() => {
+    alert ("count updated to " + a)
+  }, [a])
+  
+
 
   return (
     <>
-    HELLO WORLD
+    <p>
+
+    HELLO WORLD - {a}
+    </p>
+    <button onClick={() => changeState(a + 1)}>Click Me</button>
     </>
   )
 }
