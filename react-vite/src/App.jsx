9c38c014332b5141e5503c58931bd744b394ffc9
saatwik007 <saatwikwahi100@gmail.com>
2025-10-08 23:08:03 +0530

    Day - 50  8/10/25 - REACT

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
     HELLO WORLD - {a}
     </p>
-    <button onClick={() => changeState(a + 1)}>CLICK</button>
+    <button onClick={() => changeState(a + 1)}>CLICK ME</button>
     </>
   )
 }
