9c6036af6141ad269c5077fb8dff6c73cbb75bb1
saatwik007 <saatwikwahi100@gmail.com>
2025-10-15 00:14:28 +0530

    Day - 51  15/10/25 - REACT

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -3,22 +3,40 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
+  const [ showBtn , setShowBtn] = useState(false);
+    const [loading, setLoading] = useState(false); // Optional: loading state
+  const [todos, setTodos] = useState([]);
   
-  const [a, changeState] = useState(0); 
-  
-  useEffect(() => {
-    alert ("count updated to " + a)
-  }, [a])
-  
+    useEffect(() => {
+  setLoading(true);
+  fetch ("https://jsonplaceholder.typicode.com/todos").then(response => response.json())
+      .then(json => {
+        setTodos(json);
+        setLoading(false);
+      })
+  }, [])
+
+
+  const Todo = ({todo}) => {
+    return (
+    <div className='m-4 border-1 border-red-900 p-4'>
+    <div className='todo'>{todo.id} {todo.title}</div>
+    </div>
+    )
+  }
 
 
   return (
     <>
     <p>
-
-    HELLO WORLD - {a}
     </p>
-    <button onClick={() => changeState(a + 1)}>CLICK ME !!</button>
+    <button onClick={() => { setShowBtn(!showBtn)}}>CLICK ME !!</button>
+   {showBtn && <h1 className='text-3xl font-bold underline'>TODOS</h1>}
+     {/* Show loading state */}
+      {showBtn && loading && <p>Loading todos...</p>}
+   {showBtn && todos.map(todo => {
+     return <Todo key={todo.id} todo={todo}/>
+   })}
     </>
   )
 }
